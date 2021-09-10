bfcd2882222afe998482f16e850f37ac29fd5f5b
adit313 <app2116@gmail.com>
2021-09-10 18:46:10 -0400

    Working on clearing node

--- miner/db/seeds.rb
+++ miner/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# block1 = Block.create(block_hash: "",
+#                       commit_hash: "",
+#                       merkle_hash: "",
+#                       prev_block_hash: "",
+#                       nonce: 1,
+#                       solution_hash: block_result[1])
+# block2 = Block.create(block_hash: nil,
+#                       commit_hash: nil,
+#                       merkle_hash: merkle_tree_hash,
+#                       prev_block_hash: prev_block_hash,
+#                       nonce: 1,
+#                       solution_hash: block_result[1])
+# block3 = Block.create(block_hash: nil,
+#                       commit_hash: nil,
+#                       merkle_hash: merkle_tree_hash,
+#                       prev_block_hash: prev_block_hash,
+#                       nonce: 1,
+#                       solution_hash: block_result[1])
