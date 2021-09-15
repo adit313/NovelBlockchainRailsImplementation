f06997eaaf8ed82c10ad67a44a3593f806ffa486
adit313 <app2116@gmail.com>
2021-09-15 12:41:45 -0400

    wrote close and commit logic

--- clearing/app/models/confirmed_transaction.rb
+++ clearing/app/models/confirmed_transaction.rb
@@ -44,7 +44,7 @@ class ConfirmedTransaction < ApplicationRecord
     end
 
     #   verify the transaction hash, sender, nonce and information
-    calculated_hash = Digest::SHA256.hexdigest(parse_input["amount"].to_s + parse_input["destination"].to_s + parse_input["nonce"].to_s + parse_input["sender"].to_s + parse_input["sender_public_key"].to_s + parse_input["txn_fee"].to_s)
+    calculated_hash = Digest::SHA256.hexdigest(parse_input["amount"].to_s + parse_input["destination"].to_s + parse_input["nonce"].to_s + parse_input["sender"].to_s + parse_input["sender_public_key"].to_s + parse_input["tx_fee"].to_s)
     if calculated_hash != parse_input["transaction_hash"]
       return "The hash did not match the SHA256 hex Hash of (amount + destination + nonce + sender + sender_public_key + tx_fee)"
     end
@@ -106,7 +106,7 @@ class ConfirmedTransaction < ApplicationRecord
     }
 
     all_of_senders_uncommitted_transactions.each { |open_txn|
-      sender_balance -= open_txn.txn_fee
+      sender_balance -= open_txn.tx_fee
     }
 
     destinations = []
